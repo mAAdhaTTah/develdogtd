5775f5fb48bad6e91336874c5df56910e55e1d4a
mAAdhaTTah <jamesorodig@gmail.com>
2015-05-30 22:40:18 -0400

    Break out redis into seperate module

--- middleware/session.js
+++ middleware/session.js
@@ -1,20 +1,10 @@
 var app = require('../server');
 var session = require('express-session');
 var RedisStore = require('connect-redis')(session);
-var redis;
-
-if (process.env.REDISTOGO_URL) {
-  var rtg   = require('url').parse(process.env.REDISTOGO_URL);
-  redis = require('redis').createClient(rtg.port, rtg.hostname);
-
-  redis.auth(rtg.auth.split(":")[1]);
-} else {
-  redis = require('redis').createClient();
-}
 
 module.exports = session({
   store: new RedisStore({
-    client: redis,
+    client: require('./redis'),
     disableTTL: true
   }),
   secret: 'keyboard cat',
