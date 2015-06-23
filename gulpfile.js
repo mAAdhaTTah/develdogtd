5173dc84fb1e556448bd46b1e4d4a14c9b1b9caf
mAAdhaTTah <jamesorodig@gmail.com>
2015-06-22 23:32:16 -0400

    Add database pull support
    * user --remote="REMOTEDBURL" to pull
    * local db should be at same migration level as remote

--- gulpfile.js
+++ gulpfile.js
@@ -15,6 +15,11 @@ var config = require('./config');
 var gulpif = require('gulp-if');
 var uglify = require('gulp-uglify');
 var minifyCss = require('gulp-minify-css');
+var pg = require('pg');
+var copyTo = require('pg-copy-streams').to;
+var copyFrom = require('pg-copy-streams').from;
+var Promise = require('bluebird');
+var argv = require('yargs').argv;
 
 gulp.task('styles', function() {
   return gulp.src('styles/main.scss')
@@ -121,6 +126,83 @@ gulp.task('scripts', function() {
     .pipe(source('common.js'))
     .pipe(gulp.dest('./public'))
     .on('finish', bundle);
+});
+
+gulp.task('pull', function(cb) {
+  var remoteConn = argv.remote;
+
+  var local = new pg.Client(config.db);
+
+  local.connect();
+
+  Promise
+    .all(['users', 'tasks', 'contexts', 'knex_migrations'])
+    .map(function(table) {
+      return new Promise(function(resolve, reject) {
+        // clear out our local database
+        local.query('TRUNCATE ' + table + ' RESTART IDENTITY CASCADE', function(err) {
+          if (err) {
+            return reject(err);
+          }
+
+          return resolve(table);
+        });
+      })
+    }, {
+      concurrency: 1
+    })
+    .map(function(table) {
+      return new Promise(function(resolve, reject) {
+        console.log('Started pulling ' + table);
+        var remote = new pg.Client(remoteConn);
+        remote.connect();
+
+        var remoteStream = remote.query(copyTo('COPY (SELECT * FROM ' + table + ') TO STDOUT'))
+          .on('error', function(err) {
+            console.log('Error pulling ' + table);
+            reject(err);
+          });
+
+        var localStream = local.query(copyFrom('COPY ' + table + ' FROM STDIN'))
+          .on('finish', function() {
+            console.log('Fully received ' + table);
+            remote.end();
+            resolve(table);
+          })
+          .on('error', function(err) {
+            console.log('Error receiving ' + table);
+            console.log('Error', err);
+            reject(err);
+          });
+
+        remoteStream.pipe(localStream);
+      });
+    }, {
+      concurrency: 1
+    })
+    .map(function(table) {
+      return new Promise(function(resolve, reject) {
+        // reset the primary key sequence for all the tables
+        local.query("SELECT pg_catalog.setval(pg_get_serial_sequence('" + table + "', 'id'), (SELECT MAX(id) FROM " + table + ")+1);", function(err) {
+          if (err) {
+            return reject(err);
+          }
+
+          return resolve(table);
+        });
+      })
+    }, {
+      concurrency: 1
+    })
+    .catch(function(err) {
+      console.log('Error', err);
+      return err;
+    })
+    .then(function() {
+      console.log('Finish db pull');
+      local.end();
+      return cb();
+    });
 });
 
 gulp.task('default', ['styles', 'scripts', 'watchers']);
