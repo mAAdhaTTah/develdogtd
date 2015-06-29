eb1ef56a2eb6135728a49555bd8bfdb84355695b
mAAdhaTTah <jamesorodig@gmail.com>
2015-06-28 20:36:49 -0400

    App view cleanup and improvements

--- views/app.js
+++ views/app.js
@@ -4,37 +4,63 @@ import actionChannel from '../channels/action';
 import projectChannel from '../channels/project';
 
 export default LayoutView.extend({
+
+  /**
+   * Grabs this page element
+   */
   el: '.app',
+
+  /**
+   * Uses already rendered html
+   */
   template: false,
+
+  /**
+   * Defines regions on the page
+   */
   regions: {
-    header: '.header',
     main: '.main'
   },
 
-  initialize: function() {
-    this.$nav = this.$('.navigation');
-    this.$addAction = this.$('#add-action');
-    this.$refresh = this.$('#refresh');
-
-    this.$addAction.click(this.addAction);
-    this.$refresh.click(this.refresh);
-
-    globalChannel.comply('route', this.route, this);
+  /**
+   * UI hash
+   */
+  ui: {
+    'addAction': '#add-action',
+    'refresh': '#refresh'
   },
 
+  /**
+   * Events bindings
+   */
+  events: {
+    'click @ui.addAction': 'addAction',
+    'click @ui.refresh': 'refresh'
+  },
+
+  /**
+   * Adds a new action on click
+   */
   addAction: function() {
     actionChannel.command('add');
   },
 
+  /**
+   * Refreshes the currently shown actions on click
+   */
   refresh: function() {
     globalChannel.command('refresh');
   },
 
+  /**
+   * Sets the currently action perspective
+   * @param perspective
+   */
   setActive: function(perspective) {
     if (this.$active) {
       this.$active.removeClass('active');
     }
 
-    this.$active = this.$nav.find('#' + perspective).addClass('active');
+    this.$active = this.$('#' + perspective).addClass('active');
   }
 });
